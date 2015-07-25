68897f573b2298cbda00f7428f56d9a6c141ddd8
HristoVutov <vutov23@gmail.com>
2015-07-25 23:32:57 +0300

    Model For Database
    Added Model with Products,Vendor and Measure classes

---
+++ SupermarketsChain/SyperMarketChain.Model/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyperMarketChain.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public int VendorId { get; set; }
+
+        public virtual Vendor Vendor { get; set; }
+
+        public string ProductName { get; set; }
+
+        public int MeasureID { get; set; }
+
+        public virtual Measure Measure { get; set; }
+
+        public double Price { get; set; }
+    }
+}
