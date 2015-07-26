20961549c5bcdbe386cc65017b680246e68c3155
goodlifeinc <genata93@gmail.com>
2015-07-26 09:44:23 +0300

    Added Product collection to Measures - Measure.Products

--- SupermarketsChain/SuperMarketChain.Model/Measure.cs
+++ SupermarketsChain/SuperMarketChain.Model/Measure.cs
@@ -9,11 +9,24 @@ namespace SuperMarketChain.Model
 {
     public class Measure
     {
+        private ICollection<Product> products;
+
+        public Measure()
+        {
+            this.products = new HashSet<Product>();
+        }
+
         [Key]
         public int ID { get; set; }
 
         [Required]
         [MaxLength(20)]
         public string MeasureName { get; set; }
+
+        public virtual ICollection<Product> Products
+        {
+            get { return this.products; }
+            set { this.products = value; }
+        }
     }
 }
