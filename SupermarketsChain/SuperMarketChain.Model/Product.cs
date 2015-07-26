a8c51c4d2021969431645a8632ed4afbdeb9cc3f
goodlifeinc <genata93@gmail.com>
2015-07-26 08:59:18 +0300

    Added data annotations, changed typo in SyperMarket to SuperMarket

---
+++ SupermarketsChain/SuperMarketChain.Model/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SuperMarketChain.Model
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int VendorId { get; set; }
+
+        public virtual Vendor Vendor { get; set; }
+
+        [MaxLength(20)]
+        public string ProductName { get; set; }
+
+        [Required]
+        public int MeasureID { get; set; }
+
+        public virtual Measure Measure { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+    }
+}
---
+++ SupermarketsChain/SuperMarketChain.Model/Vendor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SuperMarketChain.Model
+{
+    public class Vendor
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string VendorName { get; set; }
+    }
+}
