f6025df772f7b8aa15d9d4ae85c24203f24bd54f
goodlifeinc <genata93@gmail.com>
2015-07-26 11:40:34 +0300

    Getting data from oracle tables into C# variables

--- SupermarketsChain/OracleToSQL/OracleToSQL.cs
+++ SupermarketsChain/OracleToSQL/OracleToSQL.cs
@@ -11,7 +11,7 @@ namespace OracleToSQL
 {
     public static class OracleToSQL
     {
-        public static void ReadTable(string host, string oraUser, string oraPass, string tableName, String[] fields) {
+        public static ICollection<Product> GetProductTable(string host, string oraUser, string oraPass, string tableName, String[] fields) {
             string oradb = "Data Source=(DESCRIPTION="
              + "(ADDRESS=(PROTOCOL=TCP)(HOST="+ host + ")(PORT=1521))"
              + "(CONNECT_DATA=(SERVICE_NAME=XE)));"
@@ -29,21 +29,98 @@ namespace OracleToSQL
 
             cmd.CommandType = CommandType.Text;
             OracleDataReader dr = cmd.ExecuteReader();
+
+            ICollection<Product> products = new HashSet<Product>();
+
             while(dr.Read()) {
-                for (int i = 0; i < fields.Length; i++)
-                {
-                    if (dr.GetDataTypeName(i).Equals("Decimal"))
-                    {
-                        Console.WriteLine(dr.GetDecimal(i));
-                    }
-                    else
-                    {
-                        Console.WriteLine(dr.GetString(i));
-                    }
-                }
+                int id = dr.GetInt32(0);
+                int vendorId = dr.GetInt32(1);
+                string productName = dr.GetString(2);
+                int measureId = dr.GetInt32(3);
+                decimal price = dr.GetDecimal(4);
+
+                Product product = new Product(id, vendorId, productName, measureId, price);
+
+                products.Add(product);
             }
 
             conn.Dispose();
+
+            return products;
+        }
+
+        public static ICollection<Measure> GetMeasureTable(string host, string oraUser, string oraPass, string tableName, String[] fields)
+        {
+            string oradb = "Data Source=(DESCRIPTION="
+             + "(ADDRESS=(PROTOCOL=TCP)(HOST=" + host + ")(PORT=1521))"
+             + "(CONNECT_DATA=(SERVICE_NAME=XE)));"
+             + "User Id=" + oraUser + ";Password=" + oraPass + ";";
+            //string oradb = "Data Source=XE;User Id=" + oraUser + ";Password=" + oraPass + ";";
+
+            OracleConnection conn = new OracleConnection(oradb);
+            conn.Open();
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+
+            cmd.CommandText = "select " +
+                string.Join(", ", fields) +
+                " from " + tableName;
+
+            cmd.CommandType = CommandType.Text;
+            OracleDataReader dr = cmd.ExecuteReader();
+
+            ICollection<Measure> measures = new HashSet<Measure>();
+
+            while (dr.Read())
+            {
+                int id = dr.GetInt32(0);
+                string measureName = dr.GetString(1);
+
+                Measure measure = new Measure(id, measureName);
+
+                measures.Add(measure);
+            }
+
+            conn.Dispose();
+
+            return measures;
+        }
+
+        public static ICollection<Vendor> GetVendorTable(string host, string oraUser, string oraPass, string tableName, String[] fields)
+        {
+            string oradb = "Data Source=(DESCRIPTION="
+             + "(ADDRESS=(PROTOCOL=TCP)(HOST=" + host + ")(PORT=1521))"
+             + "(CONNECT_DATA=(SERVICE_NAME=XE)));"
+             + "User Id=" + oraUser + ";Password=" + oraPass + ";";
+            //string oradb = "Data Source=XE;User Id=" + oraUser + ";Password=" + oraPass + ";";
+
+            OracleConnection conn = new OracleConnection(oradb);
+            conn.Open();
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+
+            cmd.CommandText = "select " +
+                string.Join(", ", fields) +
+                " from " + tableName;
+
+            cmd.CommandType = CommandType.Text;
+            OracleDataReader dr = cmd.ExecuteReader();
+
+            ICollection<Vendor> vendors = new HashSet<Vendor>();
+
+            while (dr.Read())
+            {
+                int id = dr.GetInt32(0);
+                string vendorName = dr.GetString(1);
+
+                Vendor vendor = new Vendor(id, vendorName);
+
+                vendors.Add(vendor);
+            }
+
+            conn.Dispose();
+
+            return vendors;
         }
     }
 }
--- SupermarketsChain/SuperMarketChain.Client/ConsoleClient.cs
+++ SupermarketsChain/SuperMarketChain.Client/ConsoleClient.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SuperMarketChain.Data;
+using OracleToSQL;
 
 namespace SuperMarketChain.Client
 {
@@ -14,7 +15,25 @@ namespace SuperMarketChain.Client
             var context = new SupermarketChainContext();
 
             var productCount = context.Products.Count();
+
             Console.WriteLine(productCount);
+
+            var prodFields = new String[] { "id", "vendorid", "productname", "measureid", "price" };
+
+            var prodData = OracleToSQL.OracleToSQL.GetProductTable("EVGENI-PC", "admin", "1111", "Products", prodFields);
+
+            var measFields = new String[] { "id", "measurename" };
+
+            var measData = OracleToSQL.OracleToSQL.GetMeasureTable("EVGENI-PC", "admin", "1111", "Measures", measFields);
+
+            var vendorFields = new String[] { "id", "vendorname" };
+
+            var vendorData = OracleToSQL.OracleToSQL.GetVendorTable("EVGENI-PC", "admin", "1111", "Vendors", vendorFields);
+
+            foreach (Vendor item in vendorData)
+            {
+                Console.WriteLine(item.VendorName);
+            }
         }
     }
 }
