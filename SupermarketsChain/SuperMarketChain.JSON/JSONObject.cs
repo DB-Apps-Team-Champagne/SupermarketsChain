a550caaaffc21a0f0c61fd59c2470d5324d7a5ed
HristoVutov <vutov23@gmail.com>
2015-07-30 13:19:30 +0300

    Added JSON Class Library and JsonReports

---
+++ SupermarketsChain/SuperMarketChain.JSON/JSONObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarketChain.JSON
+{
+    class JSONObject
+    {
+        public int productID { get; set; }
+
+        public string productName { get; set; }
+
+        public string vendorName { get; set; }
+
+        public double quantitySold { get; set; }
+
+        public double income { get; set; }
+    }
+}
