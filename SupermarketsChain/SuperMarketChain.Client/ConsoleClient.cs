e168b011d8180cb47d1752a90656e72d2520aed0
HristoVutov <vutov23@gmail.com>
2015-07-31 15:11:29 +0300

    Fix

--- SupermarketsChain/SuperMarketChain.Client/ConsoleClient.cs
+++ SupermarketsChain/SuperMarketChain.Client/ConsoleClient.cs
@@ -8,6 +8,7 @@ using OracleToSQL;
 using SuperMarketChain.Data.Utils;
 using SuperMarketChain.MySQL;
 using MySql.Data.MySqlClient;
+using System.Data.SQLite;
 
 namespace SuperMarketChain.Client
 {
@@ -15,21 +16,22 @@ namespace SuperMarketChain.Client
     {
         static void Main()
         {
-            var context = new SupermarketChainContext();
+            CreateSQLite();
+            //var context = new SupermarketChainContext();
 
-            var mysqlContext = new SupermarketChainMySQL(SuperMarketChain.Data.Utils.MySQL.connectionString);
+            //var mysqlContext = new SupermarketChainMySQL(SuperMarketChain.Data.Utils.MySQL.connectionString);
 
-            SuperMarketChain.Data.Utils.MySQL.CreateDBIfNonExistent();
+            //SuperMarketChain.Data.Utils.MySQL.CreateDBIfNonExistent();
 
-            SuperMarketChain.Data.Utils.MySQL.ImportVendorsFromMSSqlToMySQL(context);
+            //SuperMarketChain.Data.Utils.MySQL.ImportVendorsFromMSSqlToMySQL(context);
 
-            SuperMarketChain.Data.Utils.MySQL.ImportMeasuresFromMSSqlToMySQL(context);
+            //SuperMarketChain.Data.Utils.MySQL.ImportMeasuresFromMSSqlToMySQL(context);
 
-            SuperMarketChain.Data.Utils.MySQL.ImportProductsFromMSSqlToMySQL(context);
+            //SuperMarketChain.Data.Utils.MySQL.ImportProductsFromMSSqlToMySQL(context);
 
-            SuperMarketChain.Data.Utils.MySQL.ImportSalesReportsFromMSSqlToMySQL(context);
+            //SuperMarketChain.Data.Utils.MySQL.ImportSalesReportsFromMSSqlToMySQL(context);
 
-            SuperMarketChain.Data.Utils.MySQL.ImportExprencesFromMSSqlToMySQL(context);
+            //SuperMarketChain.Data.Utils.MySQL.ImportExprencesFromMSSqlToMySQL(context);
 
             //Excel ex = new Excel();
             //ex.unZip();
@@ -64,6 +66,40 @@ namespace SuperMarketChain.Client
 
             //GenerateSalesReportPdf.Generate(context, new DateTime(2000, 10, 10), new DateTime(2016, 10, 10));
 
+        }
+        private static void CreateSQLite()
+        {
+            SQLiteConnection.CreateFile("SQLiteDatabase.sqlite");
+            SQLiteConnection m_dbConnection =
+                    new SQLiteConnection("Data Source=SQLiteDatabase.sqlite;Version=3;");
+            m_dbConnection.Open();
+
+            string sql = "CREATE TABLE taxes (productname NVARCHAR(100), tax INT)";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            var context = new SupermarketChainContext();
+
+            var products = context.Products.Select(p => p.ProductName).Distinct();
+
+
+            var r = new Random();
+            foreach (var item in products)
+            {
+                string prodName = item;
+                string insert = "insert into taxes (productname, tax) values ('" + prodName + "', '" + r.Next(10, 30) + "')";
+
+                SQLiteCommand cmd = new SQLiteCommand(insert, m_dbConnection);
+                cmd.ExecuteNonQuery();
+            }
+
+            string taxReader = "select * from taxes order by tax desc";
+            SQLiteCommand readCommand = new SQLiteCommand(taxReader, m_dbConnection);
+            SQLiteDataReader reader = readCommand.ExecuteReader();
+            while (reader.Read())
+                Console.WriteLine("Name: " + reader["productName"] + "\tScore: " + reader["tax"] + "%");
+
+            m_dbConnection.Close();
         }
     }
 }
