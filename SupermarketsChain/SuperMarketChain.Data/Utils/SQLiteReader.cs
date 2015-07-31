236c6dfb7ed62f003d795244075c2b0aa376da76
HristoVutov <vutov23@gmail.com>
2015-07-31 14:51:33 +0300

    Add SQLite reader

---
+++ SupermarketsChain/SuperMarketChain.Data/Utils/SQLiteReader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarketChain.Data.Utils
+{
+    class SQLiteReader
+    {
+        static void Main()
+        {
+
+            SQLiteConnection m_dbConnection =
+                    new SQLiteConnection("Data Source=SQLiteDatabase.sqlite;Version=3;");
+            m_dbConnection.Open();
+
+
+
+            string taxReader = "select * from taxes order by tax desc";
+            SQLiteCommand readCommand = new SQLiteCommand(taxReader, m_dbConnection);
+            SQLiteDataReader reader = readCommand.ExecuteReader();
+            while (reader.Read())
+                Console.WriteLine("Name: " + reader["productName"] + "\tScore: " + reader["tax"] + "%");
+
+            m_dbConnection.Close();
+        }
+    }
+}
